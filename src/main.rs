a6e8f792285581e90e0ce478eb7e9b0d567dcbe6
Sergey Kozub <sergey@kozub.email>
2022-12-06 08:08:42 +0100

    day06

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,6 +21,7 @@ fn main() {
     let content = fs::read_to_string(filename).expect("Error reading input");
 
     let days: Vec<fn(&str)> = vec![
-        day01::run, day02::run, day03::run, day04::run, day05::run];
+        day01::run, day02::run, day03::run, day04::run, day05::run,
+        day06::run];
     days[number - 1](&content)
 }
