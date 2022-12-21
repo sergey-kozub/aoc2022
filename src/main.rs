20b385af4a97c0a783df074eb2505dfc98be2fec
Sergey Kozub <sergey@kozub.email>
2022-12-21 14:35:01 +0100

    day21

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ mod day17;
 mod day18;
 mod day19;
 mod day20;
+mod day21;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -38,6 +39,7 @@ fn main() {
         day01::run, day02::run, day03::run, day04::run, day05::run,
         day06::run, day07::run, day08::run, day09::run, day10::run,
         day11::run, day12::run, day13::run, day14::run, day15::run,
-        day16::run, day17::run, day18::run, day19::run, day20::run];
+        day16::run, day17::run, day18::run, day19::run, day20::run,
+        day21::run];
     days[number - 1](&content)
 }
